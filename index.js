e8870edd9df375822944b2cc3e634a9c8d5316d6
masin <masin@gmail.com>
2024-12-09 22:34:41 +0330

    time

--- index.js
+++ index.js
@@ -74,7 +74,7 @@ const times = [
     { hour: 8, minute: 0 },
     { hour: 12, minute: 0 },
     { hour: 19, minute: 0 },
-    { hour: 22, minute: 34 }
+    { hour: 22, minute: 0 }
 ];
 times.forEach(t => {
 
